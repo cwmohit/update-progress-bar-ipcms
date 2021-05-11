e5d31c8ea0440bfd917264f39839f891d45cd04b
member8lks <member8@lakshmisri.com>
2021-05-11 20:58:24 +0530

    progress update

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,29 +2,39 @@ import React from 'react';
 import './progressBar.css';
 
 const ProgressBar = () => {
-    const listProp = ["Drafting","Mentor Review","Quality Check"];
+    // const listProp = ["Drafting", "Mentor Review", "Quality Check"];
+    const listProp = [{ text: "Drafting", date: "17 May 2020" }, { text: "Mentor Review", date: "22 June 2020" }, { text: "Quality Check", date: "" }];
     const listLength = listProp.length;
-    const slideWidth = 100/listLength;
+    const slideWidth = 11*listLength;
     const slideWidthStyle = {
         width: `${slideWidth}%`
     }
+  
+    // const FirstFilteredList = listProp.filter(list => {
+    //     return list.text === "Quality Check"
+    // });
+    // const SecondFilteredList = listProp.filter(list => {
+    //     return list != "Quality Check"
+    // });
 
-    const FirstFilteredList = listProp.filter(list => {
-        return list === "Quality Check"
-    });
-    const SecondFilteredList = listProp.filter(list => {
-        return list != "Quality Check"
-    });
-    
+
+
     return (
         <div className="progress-parent">
-            <div className="progress-details">
-               {SecondFilteredList.map(list => {
+            <div className="progress-details" style={slideWidthStyle}>
+                {/* {SecondFilteredList.map(list => {
                    return <p className="list-second">{list}</p>
                })}
                {FirstFilteredList.map(list => {
                    return <p className="list-first">{list}</p>
-               })}
+               })} */}
+                {
+                   listProp && listProp.map((list) => (
+                        <p key={list.text} className={list.text==='Quality Check'? "Quality": "list"} >
+                            {list.text} {list.date}
+                        </p>
+                    ))
+                }
             </div>
             {/* <div className="progress-details">
                <p className="drafting-date"> 
@@ -39,9 +49,10 @@ const ProgressBar = () => {
             </div> */}
             <div className="progressbar">
                 <div className="bar-width" style={slideWidthStyle}>
-                    <div className="circle-start"></div>
-                    <div className="circle-mid"></div>
-                    <div className="circle-end"> </div>
+                    {
+                       listProp.map(list=>  <div className="circle"></div>)
+                    }
+                 
                 </div>
             </div>
         </div>
